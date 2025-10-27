f14e21c3642255b6a3c320a14132c5b0dc0e09a8
Студент <student@kvptk.local>
2025-10-27 16:26:34 +0500

    first commit

---
+++ flutter_application_1/lib/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final myTheme = ThemeData(
+  scaffoldBackgroundColor: Color.fromARGB(255, 185, 185, 185),
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 55, 0, 255),
+  ),
+  textTheme: TextTheme(
+    titleLarge: TextStyle(fontSize: 32, color: Colors.white)
+  ),
+);
